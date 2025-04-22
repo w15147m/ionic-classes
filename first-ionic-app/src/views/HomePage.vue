fc072dcda513e4ed27f638df093b6f89934408a6
waseem abbas <waseemofficee@gmail.com>
2025-04-22 09:02:13 +0500

    chore: remove all project files and configurations
    - Deleted all source files, including Vue components, routing, and styles.
    - Removed configuration files for TypeScript, Vite, ESLint, and Cypress.
    - Cleared package.json and package-lock.json, resetting project dependencies.
    - Eliminated test files and fixtures for unit and e2e testing.

--- first-ionic-app/src/views/HomePage.vue
+++ first-ionic-app/src/views/HomePage.vue
@@ -2,35 +2,33 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-menu-button color="primary"></ion-menu-button>
-        </ion-buttons>
-        <ion-title>{{ $route.params.id }}</ion-title>
+        <ion-title>Blank</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">{{ $route.params.id }}</ion-title>
+          <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
 
       <div id="container">
-        <strong class="capitalize">{{ $route.params.id }}</strong>
-        <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <strong>Ready to create an app?</strong>
+        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 </script>
 
 <style scoped>
 #container {
   text-align: center;
+  
   position: absolute;
   left: 0;
   right: 0;
@@ -46,7 +44,9 @@ import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, Io
 #container p {
   font-size: 16px;
   line-height: 22px;
+  
   color: #8c8c8c;
+  
   margin: 0;
 }
 
